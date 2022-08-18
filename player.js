6c9a61151d7b6e26a40c96261e05e5108ae025da
saints456 <snt456@hotmail.com>
2022-08-18 01:28:40 -0500

    Se crea github project

---
+++ player.js
@@ -0,0 +1,97 @@
+import { Sitting,Running,Jumping,Falling,Rolling,Diving,HIT } from './playerStates.js';
+import {CollisionAnimation} from './collisionAnimation.js';
+import {FloatingMessage} from './floatingMessages.js';
+
+export class Player{
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height= 92;
+        this.x= 0;
+        this.y= this.game.height - this.height - this.game.groundMargin;
+        this.vy = 0;
+        this.weigth = 1;
+        this.image = document.getElementById('Player');
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame;
+        this.fps = 20;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
+        this.speed = 0;
+        this.maxSpeed = 10;        
+        this.states = [new Sitting(this.game), new Running(this.game),new Jumping(this.game),
+            new Falling(this.game), new Rolling(this.game),new Diving(this.game),new HIT(this.game)];        
+            this.currentState = null;
+    }
+    update(input,deltaTime){
+        this.checkCollision();        
+        this.currentState.handleInput(input);
+        //horizontal movement
+        this.x += this.speed;
+        if(input.includes('ArrowRight') && this.currentState !== this.states[6]) 
+        this.speed = this.maxSpeed;
+        else if(input.includes('ArrowLeft') && this.currentState !== this.states[6]) 
+        this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        if(this.x < 0) this.x = 0;
+        if(this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+        //verticalmovement
+        //if(input.includes('ArrowUp') && this.onGround()) this.vy -= 30;
+        this.y += this.vy;
+        if(!this.onGround()) this.vy += this.weigth;
+        else this.vy = 0;
+        
+        //Verticla boundaries
+        if(this.y > this.game.height - this.height - this.game.groundMargin) this.y = this.game.height
+        -this.height - this.game.groundMargin;
+
+        //sprite animation
+        if(this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if(this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        }else{
+            this.frameTimer += deltaTime;
+        }
+        //if(this.frameX < this.maxFrame) this.frameX++;
+        //else this.frameX = 0;
+    }
+    draw(context){
+        if(this.game.debug) context.strokeRect(this.x,this.y,this.width,this.height);
+        context.drawImage(this.image,this.frameX * this.width,this.frameY * this.height,this.width,this.height,this.x,this.y,this.width,this.height);
+    }
+    onGround(){
+        return this.y >= this.game.height - this.height - this.game.groundMargin;
+    }
+    setState(state,speed){
+        this.currentState = this.states[state];
+        this.game.speed = speed * this.game.maxSpeed;
+        this.currentState.enter();        
+        console.log(this.currentState);
+    }
+    checkCollision(){
+        this.game.enemies.forEach(enemy => {
+            if(
+                enemy.x < this.x + this.width &&
+                enemy.x + enemy.width > this.x &&
+                enemy.y < this.y + this.height &&
+                enemy.y + enemy.height > this.y
+            ){
+                //Collision detected
+                enemy.markedForDeletion = true;
+                this.game.collisions.push(new CollisionAnimation(this.game,
+                    enemy.x + enemy.width * 0.5,enemy.y + enemy.height * 0.5));
+                if(this.currentState === this.states[4] || this.currentState === this.states[5]){
+                    this.game.score++;
+                    this.game.floatingMessages.push(new FloatingMessage("+1",enemy.x,enemy.y,150,50));
+                }else{
+                    this.setState(6,0);
+                    this.game.score -=5;
+                    this.game.lives--;
+                    if(this.game.lives <= 0) this.game.gameOver = true;
+                }
+            }
+        });
+    }
+}
